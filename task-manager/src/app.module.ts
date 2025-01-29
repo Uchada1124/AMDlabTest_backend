c3edd4ed40cbbc10fced4035aef90e8a108c146b
Uchada1124 <uchidadaiki1124@gmail.com>
2025-01-29 16:04:14 +0900

    docker

--- task-manager/src/app.module.ts
+++ task-manager/src/app.module.ts
@@ -3,16 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'user',
-      password: 'password',
-      database: 'task_db',
+      type: "postgres",
+      host: process.env.DATABASE_HOST || "localhost",
+      port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+      username: process.env.DATABASE_USER || "user",
+      password: process.env.DATABASE_PASSWORD || "password",
+      database: process.env.DATABASE_NAME || "task_db",
       autoLoadEntities: true,
       synchronize: true,
     }),
